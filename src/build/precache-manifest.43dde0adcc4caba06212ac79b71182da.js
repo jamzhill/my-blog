45e77bf3c9c451eeb9a0cb06e63f56eff03a828e
james hill <james@jamess-iMac.local>
2019-07-23 16:25:16 -0700

    initial commit

---
+++ src/build/precache-manifest.43dde0adcc4caba06212ac79b71182da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c999296cabda71ba8f4d4103195792e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7166bba1e12b1fa86cb",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "65525d98e6835d505d2f",
+    "url": "/static/js/2.f5e6359f.chunk.js"
+  },
+  {
+    "revision": "b7166bba1e12b1fa86cb",
+    "url": "/static/js/main.6907e8bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
